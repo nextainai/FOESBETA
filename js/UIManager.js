3375be87352a4bb302c640db76a3204ae61e8a86
Next Ai <dasritam288@gmail.com>
2026-03-20 12:59:23 +0530

    Update UIManager.js

--- js/UIManager.js
+++ js/UIManager.js
@@ -1,4 +1,15 @@
 export default class UIManager {
+  constructor() {
+    this.crosshair = document.getElementById("crosshair");
+  }
+
+  hitMarker() {
+    this.crosshair.style.background = "red";
+    setTimeout(() => {
+      this.crosshair.style.background = "transparent";
+    }, 100);
+  }
+
   update(player, weapon) {
     document.getElementById("health").innerText = "HP: " + player.health;
     document.getElementById("ammo").innerText = "Ammo: " + weapon.ammo;
