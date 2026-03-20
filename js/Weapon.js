a174e8ea9e5a401dca8afb1904d99be24554570c
Next Ai <dasritam288@gmail.com>
2026-03-20 12:52:42 +0530

    Update Weapon.js

--- js/Weapon.js
+++ js/Weapon.js
@@ -1,53 +1,25 @@
-import { WEAPONS } from './config.js';
+export default class Weapon {
+  constructor(type) {
+    this.type = type;
 
-export class Weapon {
-    constructor(type) {
-        this.stats = WEAPONS[type];
-        this.currentAmmo = this.stats.magSize;
-        this.totalAmmo = 90;
-        this.lastShot = 0;
-        this.isReloading = false;
-        this.recoil = 0;
-        this.recoilRecovery = 0.12;
+    if (type === "rifle") {
+      this.ammo = 30;
+      this.damage = 10;
+    } else {
+      this.ammo = 12;
+      this.damage = 20;
     }
+  }
 
-    shoot(camera, raycaster, enemies, onHit) {
-        const now = Date.now();
-        if (this.isReloading || this.currentAmmo <= 0 || now - this.lastShot < this.stats.fireRate * 1000) return false;
-
-        this.lastShot = now;
-        this.currentAmmo--;
-        this.recoil += this.stats.recoil;
-
-        // Add recoil to camera
-        camera.rotation.x += this.recoil * 0.4;
-
-        raycaster.setFromCamera(new THREE.Vector2(0, 0), camera);
-        const hits = raycaster.intersectObjects(enemies);
-
-        if (hits.length > 0) {
-            const bot = enemies.find(b => b.mesh === hits[0].object);
-            if (bot) onHit(bot, this.stats.damage);
-            return true;
-        }
-        return false;
+  shoot() {
+    if (this.ammo > 0) {
+      this.ammo--;
+      return true;
     }
+    return false;
+  }
 
-    reload(callback) {
-        if (this.isReloading || this.currentAmmo === this.stats.magSize) return;
-        this.isReloading = true;
-        
-        setTimeout(() => {
-            this.currentAmmo = this.stats.magSize;
-            this.isReloading = false;
-            if (callback) callback();
-        }, this.stats.reloadTime);
-    }
-
-    update() {
-        if (this.recoil > 0) {
-            this.recoil -= this.recoilRecovery;
-            if (this.recoil < 0) this.recoil = 0;
-        }
-    }
+  reload() {
+    this.ammo = this.type === "rifle" ? 30 : 12;
+  }
 }
