7ebb480a02d1ea034e5f70e293aa21170be52a36
Next Ai <dasritam288@gmail.com>
2026-03-20 12:58:20 +0530

    Update Weapon.js

--- js/Weapon.js
+++ js/Weapon.js
@@ -5,18 +5,32 @@ export default class Weapon {
     if (type === "rifle") {
       this.ammo = 30;
       this.damage = 10;
+      this.fireRate = 100;
+      this.recoil = 0.02;
     } else {
       this.ammo = 12;
       this.damage = 20;
+      this.fireRate = 400;
+      this.recoil = 0.05;
     }
+
+    this.lastShot = 0;
   }
 
-  shoot() {
-    if (this.ammo > 0) {
-      this.ammo--;
-      return true;
-    }
-    return false;
+  canShoot() {
+    return Date.now() - this.lastShot > this.fireRate;
+  }
+
+  shoot(camera) {
+    if (this.ammo <= 0 || !this.canShoot()) return false;
+
+    this.lastShot = Date.now();
+    this.ammo--;
+
+    // recoil
+    camera.rotation.x -= this.recoil;
+
+    return true;
   }
 
   reload() {
