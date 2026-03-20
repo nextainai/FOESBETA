7d463c787d3f45e86d837fec1bf6f8966211f02d
Next Ai <dasritam288@gmail.com>
2026-03-20 12:07:57 +0530

    Update Weapon.js

--- js/Weapon.js
+++ js/Weapon.js
@@ -1,58 +1,50 @@
 import { WEAPONS } from './config.js';
 
 export class Weapon {
-    constructor(type, audioManager) {
+    constructor(type) {
         this.stats = WEAPONS[type];
         this.currentAmmo = this.stats.magSize;
         this.totalAmmo = 90;
         this.lastShot = 0;
         this.isReloading = false;
         this.recoil = 0;
-        this.recoilRecovery = 0.1;
-        this.audioManager = audioManager;
+        this.recoilRecovery = 0.12;
     }
 
-    shoot(camera, raycaster, enemies, onHit, onMiss) {
+    shoot(camera, raycaster, enemies, onHit) {
         const now = Date.now();
         if (this.isReloading || this.currentAmmo <= 0 || now - this.lastShot < this.stats.fireRate * 1000) return false;
 
         this.lastShot = now;
         this.currentAmmo--;
         this.recoil += this.stats.recoil;
-        
-        if (this.audioManager) this.audioManager.playShoot();
 
         // Add recoil to camera
-        camera.rotation.x += this.recoil * 0.5;
+        camera.rotation.x += this.recoil * 0.4;
 
         raycaster.setFromCamera(new THREE.Vector2(0, 0), camera);
-        const intersects = raycaster.intersectObjects(enemies);
+        const hits = raycaster.intersectObjects(enemies);
 
-        if (intersects.length > 0) {
-            const hitObj = intersects[0].object;
-            onHit(hitObj, this.stats.damage);
+        if (hits.length > 0) {
+            const bot = enemies.find(b => b.mesh === hits[0].object);
+            if (bot) onHit(bot, this.stats.damage);
             return true;
-        } else {
-            onMiss();
-            return false;
         }
+        return false;
     }
 
     reload(callback) {
         if (this.isReloading || this.currentAmmo === this.stats.magSize) return;
         this.isReloading = true;
         
-        if (this.audioManager) this.audioManager.playReload();
-        
         setTimeout(() => {
             this.currentAmmo = this.stats.magSize;
             this.isReloading = false;
-            if(callback) callback();
+            if (callback) callback();
         }, this.stats.reloadTime);
     }
 
     update() {
-        // Recover recoil over time
         if (this.recoil > 0) {
             this.recoil -= this.recoilRecovery;
             if (this.recoil < 0) this.recoil = 0;
